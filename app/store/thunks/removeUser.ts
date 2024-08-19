379889be281d9bf894ba01c2474dd9f11dafec86
ehab yanni <ehabayanni@gmail.com>
2024-08-19 15:25:15 +0200

    add remove user thunk function

--- app/store/thunks/removeUser.ts
+++ app/store/thunks/removeUser.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const removeUser = createAsyncThunk("users/remove", async (user: any) => {
     const response = await axios.delete(`http://localhost:3005/users/${user?.id}`);
-    return response.data;
+    return user;
 });
 
 export { removeUser };
